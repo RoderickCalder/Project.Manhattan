5996dc39f0e71a0351f90c32a752a76b3b2301e6
CL4U\Roderick.Calder <Roderick.Calder@zuto.com>
2016-11-09 09:28:50 +0000

    Moving rest client to specific place

--- Project.Manhattan/Services/MessageSend.cs
+++ Project.Manhattan/Services/MessageSend.cs
@@ -12,27 +12,7 @@ namespace Project.Manhattan1.Services
 {
     public class MessageSend : IMessageSend
     {
-        private const string URL = "https://project-manhattan-65c6a.firebaseio.com/user.json";
-        private string urlPramaters = "?auth=9Cg1ESEEm5wer6PpBAkMMc8EsUww1xr3b6amsqRO";
-        public bool getMessages()
-        {
-            HttpClient client  = new HttpClient();
-            client.BaseAddress = new Uri(URL);
-
-            HttpResponseMessage response = client.GetAsync(urlPramaters).Result;
-
-            if (response.IsSuccessStatusCode)
-            {
-                String tmp = "YAY";
-                return true;
-            }
-            else
-            {
-                String mtp = "boo";
-                return false;
-            }
-
-        }
+        
 
 
     }
