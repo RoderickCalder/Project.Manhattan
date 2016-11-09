3c9a3b6c3cad822bd3a1f867845b727a5eae1bb5
Terry Ryley <terry_ryley@waters.com>
2016-11-09 11:24:34 +0000

    Has a working MVC project

---
+++ Project.Manhattan1/Services/IMessageSend.cs
@@ -0,0 +1,9 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace Project.Manhattan1.Services
+{
+    public interface IMessageSend
+    {
+        bool getMessages();
+    }
+}
---
+++ Project.Manhattan1/Services/MessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using Newtonsoft.Json;
+using Project.Manhattan1.Models;
+
+namespace Project.Manhattan1.Services
+{
+    public class MessageHandler : IMessageHandler
+    {
+        public bool MessageSend(MessageModel messageModel)
+        {
+            var messageSend = new MessageSend();
+            return messageSend.getMessages();
+            ;
+        }
+
+        public MessageModel MessageGet(DateTime dateTime)
+        {
+            var message = new MessageModel();
+            return message;
+        }
+
+        public MessageModel DataManip(MessageModel messageModel)
+        {
+            return messageModel;
+        }
+
+        public bool Handling(MessageModel message)
+        {
+            return true;
+        }
+    }
+}
