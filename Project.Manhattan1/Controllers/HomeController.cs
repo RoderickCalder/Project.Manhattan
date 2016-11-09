77998aaf107b17e303e2fad7ffaf89c06b706bcc
Terry Ryley <terry_ryley@waters.com>
2016-11-09 12:35:08 +0000

    Added posting to the cloud

--- Project.Manhattan1/Controllers/HomeController.cs
+++ Project.Manhattan1/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Project.Manhattan1.Services;
+using Project.Manhattan1.Models;
 
 namespace Project.Manhattan1.Controllers
 {
@@ -13,17 +15,11 @@ namespace Project.Manhattan1.Controllers
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Index(MessageModel messageModel)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
+            var messageSent = new MessageHandler();
+            messageSent.MessageSend(messageModel);
             return View();
         }
     }
