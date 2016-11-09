7d8cb08206928c88321a862995eeaa2d67d53a85
Terry Ryley <terry_ryley@waters.com>
2016-11-09 11:31:16 +0000

    Removed Backup

--- Project.Manhattan - Copy/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project.Manhattan1.MvcApplication" Language="C#" %>
--- Project.Manhattan - Copy/Services/IMessageSend.cs
+++
@@ -1,9 +0,0 @@
-using System.Security.Cryptography.X509Certificates;
-
-namespace Project.Manhattan1.Services
-{
-    public interface IMessageSend
-    {
-        bool getMessages();
-    }
-}
